1582840032b82bf1ddf19ad46c7ef42d08777d8e
zcsure <592190660@qq.com>
2019-11-26 11:25:22 +0800

    celebrities message
    celebrities message
    
    celebrities second commit
    
    celebrities commit
    
    celebrities commit
    
    Update test_extract.py
    
    Update celebrity.py
    
    Update util.py
    
    Update spider.py
    
    Update spider.py
    
    Update test_extract.py
    
    Update celebrity.py

--- doubanspider/extract/celebrity.py
+++ doubanspider/extract/celebrity.py
@@ -1,5 +1,45 @@
 # 演职员表
+import re
 
 
-def extract_celebrities():
-    pass
+def _collect_staff_list(info):
+    """
+    analyse celebrities' message
+    :param info: 某种演职人员的所有人员信息
+    :return:
+    """
+    one_staff_list = list()
+    count = 1
+    for content in info:
+        one_staff = dict()
+        one_staff['name'] = content.a.text # 职员姓名
+        works = content.select('span[class="works"]') #代表作的列表
+        if works: # 防止works为空导致数组越界
+            one_staff['works'] = [work.text for work in works[0].find_all('a')]
+        role = content.select('span[class="role"]')
+        if role and role[0].text[:2] == "演员":
+            role = role[0].text
+            obj = re.search(r'[(](.*?)[)]', role)
+            if obj:
+                one_staff['character'] = obj.group(1)[2:]
+        one_staff['index'] = count #每种职员的数量
+        one_staff['link'] = content.a['href'] #职员的主页连接
+        one_staff_list.append(one_staff)
+        count += 1
+    return one_staff_list
+
+
+def extract_celebrities(items):
+    """
+    Extract celebrities of the movie.
+    :param items: 每一类演职人员信息组成的列表
+    :return: dict[staff: value], the celebrities attributes of the movie
+    """
+    staff_list = dict()
+    for item in items:
+        position = item.h2.text # 演职人员种类
+        title = position.split()[1]  # 获取职位
+        info = item.select('div[class="info"]') #这一类中每个演职人员信息组成的列表
+        one_staff = _collect_staff_list(info)
+        staff_list[title] = one_staff
+    return staff_list
--- doubanspider/test_extract.py
+++ doubanspider/test_extract.py
@@ -1,7 +1,7 @@
 import unittest
-
 from .spider import DoubanSpider
 from .extract import *
+
 
 
 class TestExtractors(unittest.TestCase):
@@ -10,6 +10,7 @@ class TestExtractors(unittest.TestCase):
     def setUpClass(cls):
         cls.spider = DoubanSpider()
         cls.content, cls.selector = cls.spider.access_brief('https://movie.douban.com/subject/26786669/')
+        
 
     def test_extract_title(self):
         title, year = extract_title(self.content)
@@ -17,9 +18,16 @@ class TestExtractors(unittest.TestCase):
         self.assertEqual(year, 2019)
 
     def test_extract_summary(self):
+
         summary = extract_summary(self.selector)
         self.assertTrue(len(summary) > 0)
 
     def test_extract_info(self):
         info = extract_info(self.content)
         self.assertEqual(len(info), 11)
+
+    def test_extract_celebrity(self):
+        celebrities = self.spider.access_celebrity('26786669')
+        staff_list = extract_celebrities(celebrities)
+        self.assertTrue(staff_list['Director'])
+
