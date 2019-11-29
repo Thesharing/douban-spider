2e2faac5a88e336fd78cc4543a4e3822444c386c
gzl <gaozheling@pku.edu.cn>
2019-11-29 17:51:56 +0800

    data score

--- doubanspider/test_extract.py
+++ doubanspider/test_extract.py
@@ -2,6 +2,7 @@ import unittest
 
 from .spider import DoubanSpider
 from .extract import *
+from doubanspider.extract.score import *
 
 
 class TestExtractors(unittest.TestCase):
@@ -23,3 +24,19 @@ class TestExtractors(unittest.TestCase):
     def test_extract_info(self):
         info = extract_info(self.content)
         self.assertEqual(len(info), 11)
+
+    def test_extract_score(self):
+        total_score = extract_score(self.content)['总评分']
+        number_of_reviewers = extract_score(self.content)['评价人数']
+        one = extract_score(self.content)['详情']['1']
+        two = extract_score(self.content)['详情']['2']
+        three = extract_score(self.content)['详情']['3']
+        four = extract_score(self.content)['详情']['4']
+        five = extract_score(self.content)['详情']['5']
+        self.assertEqual(type(total_score), int)
+        self.assertEqual(type(number_of_reviewers), int)
+        self.assertEqual(type(one), int)
+        self.assertEqual(type(two), int)
+        self.assertEqual(type(three), int)
+        self.assertEqual(type(four), int)
+        self.assertEqual(type(five), int)
