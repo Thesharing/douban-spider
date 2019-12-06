afce18325fe997a7e823127ed40137cdef00c0f8
GZL <gaozheling@pku.edu.cn>
2019-12-06 21:27:07 +0800

    优化代码

--- doubanspider/extract/score.py
+++ doubanspider/extract/score.py
@@ -8,48 +8,19 @@ def extract_score(content):
         :param content: the content extracted by BeautifulSoup
         :return: dict[name: value], {'总评分','评价人数','详情'{'1','2','3','4','5'}}
     """
-
-    res = {'详情': {}}
+    star = []
     pattern = re.compile(r'\d+')
     decimal = re.compile(r'\d+.*\d+')
     rating_num = content.find('strong', attrs={"class": "ll rating_num"})
     rating_people = content.find('a', attrs={"class": "rating_people"})
+    reviewers = rating_people.find(attrs={"property": "v:votes"})
     stars = content.find_all('span', attrs={"class": "rating_per"})
-    if decimal.findall(rating_num.string)[0]:
-        total_score = float(decimal.findall(rating_num.string)[0])
-    else:
-        total_score = 0
-    if pattern.findall(rating_people.find(attrs={"property": "v:votes"}).string)[0]:
-        number_of_reviewers = int(pattern.findall(rating_people.find(attrs={"property": "v:votes"}).string)[0])
-    else:
-        number_of_reviewers = 0
-    if decimal.findall(stars[4].string)[0]:
-        one = float(decimal.findall(stars[4].string)[0])
-    else:
-        one = 0
-    if decimal.findall(stars[3].string)[0]:
-        two = float(decimal.findall(stars[3].string)[0])
-    else:
-        two = 0
-    if decimal.findall(stars[2].string)[0]:
-        three = float(decimal.findall(stars[2].string)[0])
-    else:
-        three = 0
-    if decimal.findall(stars[1].string)[0]:
-        four = float(decimal.findall(stars[1].string)[0])
-    else:
-        four = 0
-    if decimal.findall(stars[0].string)[0]:
-        five = float(decimal.findall(stars[0].string)[0])
-    else:
-        five = 0
+    total_score = decimal.findall(rating_num.string)[0] if (decimal.findall(rating_num.string)[0]) else 0
+    number_of_reviewers = pattern.findall(reviewers.string[0]) if (pattern.findall(reviewers.string)[0]) else 0
 
-    res['总评分'] = total_score
-    res['评价人数'] = number_of_reviewers
-    res['详情']['1'] = one
-    res['详情']['2'] = two
-    res['详情']['3'] = three
-    res['详情']['4'] = four
-    res['详情']['5'] = five
+    for i in range(5):
+        star.append(decimal.findall(stars[4-i].string)[0] if(decimal.findall(stars[4-i].string)[0]) else 0)
+
+    res = {'总评分': total_score, '评价人数': number_of_reviewers, '详情': {'1': star[0], '2': star[1], '3': star[2], '4': star[3], '5': star[4]}}
 
     return res
--- doubanspider/test_extract.py
+++ doubanspider/test_extract.py
@@ -1,8 +1,8 @@
 import unittest
 
-from doubanspider.spider import DoubanSpider
-from doubanspider.extract import *
-from doubanspider.extract.score import *
+from .spider import DoubanSpider
+from .extract import *
+from .extract.score import *
 
 
 class TestExtractors(unittest.TestCase):
@@ -26,22 +26,6 @@ class TestExtractors(unittest.TestCase):
         self.assertEqual(len(info), 11)
 
     def test_extract_score(self):
-        total_score = extract_score(self.content)['总评分']
-        number_of_reviewers = extract_score(self.content)['评价人数']
-        one = extract_score(self.content)['详情']['1']
-        two = extract_score(self.content)['详情']['2']
-        three = extract_score(self.content)['详情']['3']
-        four = extract_score(self.content)['详情']['4']
-        five = extract_score(self.content)['详情']['5']
         res = extract_score(self.content)
         print(res)
-        self.assertEqual(type(total_score), float)
-        self.assertEqual(type(number_of_reviewers), int)
-        self.assertEqual(type(one), float)
-        self.assertEqual(type(two), float)
-        self.assertEqual(type(three), float)
-        self.assertEqual(type(four), float)
-        self.assertEqual(type(five), float)
-
-if __name__=="__main__":
-    unittest.main()
+        self.assertEqual(type(res), dict)
